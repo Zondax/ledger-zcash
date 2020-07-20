7fbc9a1c7f524189ee289f7fb52220817e1b31fd
Juan Leni <lenijuan@gmail.com>
2020-07-20 23:51:40 +0200

    upgrade zemu + tests

--- tests_zemu/tests/test.js
+++ tests_zemu/tests/test.js
@@ -87,7 +87,10 @@ describe('Basic checks', function () {
             const app = new ZCashApp(sim.getTransport());
 
             const addrRequest = app.showAddressAndPubKey("m/44'/133'/5'/0/1", true);
-            await Zemu.sleep(1000);
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
+
+            await sim.clickRight();
             await sim.clickRight();
             await sim.clickBoth();
 
@@ -136,7 +139,9 @@ describe('Basic checks', function () {
             const app = new ZCashApp(sim.getTransport());
 
             const addrRequest = app.showAddressAndPubKey("m/44'/133'/5'/0'/1");
-            await Zemu.sleep(2000);
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
+
             await sim.clickRight();
             await sim.clickRight();
             await sim.clickRight();
@@ -168,7 +173,8 @@ describe('Basic checks', function () {
             const app = new ZCashApp(sim.getTransport());
             const blob = Buffer.from("010000000107578c9aff7cfd240c36fa1400ee130d540f4c3397d24e8bea50a7f061116a87010000006a473044022011aecead8f48e3b342856a8da2f30c4e05bec5dc147a5bc7b382d01bf68ae5c302204126fd77522ae311a88688bce967532456b08c94322ba182a18fb7786e696c610121027e563beec6765850071067e4fcc7a46d00cbb0d675ef8df1b8d15aaeef91a21fffffffff021cbb0100000000001976a91461aac8b58ac880a45fb06eeedfcf3017679778a988ac32432400000000001976a9144fc16e1766808c0ab090be4376cea9d3a0bbe12988ac00000000", "hex");
             const signatureRequest = app.sign("m/44'/133'/5'/0/0", blob);
-            await Zemu.sleep(1000);
+            // Wait until we are not in the main menu
+            await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
             // Click right + double
             await sim.clickRight();
