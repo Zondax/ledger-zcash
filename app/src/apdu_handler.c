3c2863ee62f4c8432618464adb4e10be8bd1b067
Cryptography-Compass <lgrootbr@gmail.com>
2021-01-08 11:55:37 +0100

    cleanup/empty testfunction

--- app/src/apdu_handler.c
+++ app/src/apdu_handler.c
@@ -402,58 +402,8 @@ __Z_INLINE void handleSignSapling(volatile uint32_t *flags,
 }
 
 #if defined(APP_TESTING)
-#define LMULSIZE sizeof(long long)
-
-long long lmultest(long long a, long long b) {
-    char result[2 * LMULSIZE];
-    cx_math_mult((unsigned char *) &result, (unsigned char *) &a, (unsigned char *) &b, LMULSIZE);
-    zemu_log_stack("lmul");
-    return *((long long *) result);
-}
 
 void handleTest(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
-    long long a = 123456778;
-    long long b = 25512345;
-
-    long long aa = ((a & 0xff) << 56)
-            | ((a >> 8 & 0xff) << 48)
-            | ((a >> 16 & 0xff) << 40)
-            | ((a >> 24 & 0xff) << 32)
-            | ((a >> 32 & 0xff) << 24)
-            | ((a >> 40 & 0xff) << 16)
-            | ((a >> 48 & 0xff) << 8)
-            | ((a >> 56 & 0xff) << 0);
-
-    long long bb = ((b & 0xff) << 56)
-            | ((b >> 8 & 0xff) << 48)
-            | ((b >> 16 & 0xff) << 40)
-            | ((b >> 24 & 0xff) << 32)
-            | ((b >> 32 & 0xff) << 24)
-            | ((b >> 40 & 0xff) << 16)
-            | ((b >> 48 & 0xff) << 8)
-            | ((b >> 56 & 0xff) << 0);
-    char result[2 * LMULSIZE];
-    MEMZERO(result, 2 * LMULSIZE);
-
-    cx_math_mult((unsigned char *) result, (unsigned char *) &aa, (uint8_t *) &bb, LMULSIZE);
-
-    long long r_le = 0;
-    r_le |= result[LMULSIZE]; r_le <<= 8;
-    r_le |= result[LMULSIZE + 1]; r_le <<= 8;
-    r_le |= result[LMULSIZE + 2]; r_le <<= 8;
-    r_le |= result[LMULSIZE + 3]; r_le <<= 8;
-    r_le |= result[LMULSIZE + 4]; r_le <<= 8;
-    r_le |= result[LMULSIZE + 5]; r_le <<= 8;
-    r_le |= result[LMULSIZE + 6]; r_le <<= 8;
-    r_le |= result[LMULSIZE + 7];
-
-    if (r_le == a * b){
-        G_io_apdu_buffer[0] = 0xaa;
-    }else{
-        G_io_apdu_buffer[0] = 0xbb;
-    };
-
-    *tx = 1;
 }
 #endif
 
