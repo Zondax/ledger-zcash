024c053e6e7b9cfa531bd4ce34bc52f315b2dd7d
emmanuelm41 <emmanuelm41@gmail.com>
2024-07-19 13:17:13 -0300

    fix: build process

--- app/rust/include/commitments.h
+++ app/rust/include/commitments.h
@@ -4,7 +4,7 @@
 
 void compute_nullifier(uint8_t *ncm_ptr, uint64_t note_pos, const uint8_t *nsk_ptr, uint8_t *out_ptr);
 
-void compute_note_commitment(
+void compute_note_commitment_u(
     const uint8_t *rcm_ptr, const uint64_t value, const uint8_t *diversifier_ptr, const uint8_t *pkd, uint8_t *out_ptr);
 
 void compute_note_commitment_fullpoint(
