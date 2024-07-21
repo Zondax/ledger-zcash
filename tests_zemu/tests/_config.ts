de9bd92aa7d01a422ab556e4df03c6b198556759
emmanuelm41 <emmanuelm41@gmail.com>
2024-07-21 19:51:10 -0300

    chore: leave only nanosp for testing

--- tests_zemu/tests/_config.ts
+++ tests_zemu/tests/_config.ts
@@ -9,9 +9,10 @@ const APP_PATH_X = resolve('../app/output/app_x.elf')
 const APP_PATH_SP = resolve('../app/output/app_s2.elf')
 const APP_PATH_ST = resolve('../app/output/app_stax.elf')
 
+// FIXME Enable all models again
 export const models: IDeviceModel[] = [
-  { name: 'nanos', prefix: 'S', path: APP_PATH_S },
-  { name: 'nanox', prefix: 'X', path: APP_PATH_X },
+  //{ name: 'nanos', prefix: 'S', path: APP_PATH_S },
+  //{ name: 'nanox', prefix: 'X', path: APP_PATH_X },
   { name: 'nanosp', prefix: 'SP', path: APP_PATH_SP },
   // { name: 'stax', prefix: 'ST', path: APP_PATH_ST },
 ]
