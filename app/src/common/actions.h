8f72664344a971d8bf704e58bcb07e424010dab7
neithanmo <neithanmo@gmail.com>
2024-07-22 14:44:57 -0600

    some enhancements and updates due to changes in other parts of the code base

--- app/src/common/actions.h
+++ app/src/common/actions.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *   (c) 2018 -2022 Zondax AG
+ *   (c) 2018 -2024 Zondax AG
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,56 +15,49 @@
  ********************************************************************************/
 #pragma once
 
-#include "apdu_codes.h"
-#include "app_main.h"
-#include "coin.h"
-#include "crypto.h"
-#include "nvdata.h"
-#include "parser.h"
-#include "tx.h"
-#include "apdu_errors.h"
 #include <os_io_seproxyhal.h>
 #include <stdint.h>
 
-typedef struct {
-  address_kind_e kind;
-  uint8_t len;
-} address_state_t;
-
-typedef struct {
-  key_type_e kind;
-  uint8_t len;
-} key_state_t;
+#include "addr.h"
+#include "apdu_codes.h"
+#include "apdu_errors.h"
+#include "app_main.h"
+#include "coin.h"
+#include "crypto.h"
+#include "key.h"
+#include "nvdata.h"
+#include "parser.h"
+#include "tx.h"
 
 extern address_state_t action_addrResponse;
 extern key_state_t key_state;
 
 __Z_INLINE void app_reject() {
-  transaction_reset();
-  MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
-  view_tx_state();
-  set_code(G_io_apdu_buffer, 0, APDU_CODE_COMMAND_NOT_ALLOWED);
-  io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
+    transaction_reset();
+    MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
+    view_tx_state();
+    set_code(G_io_apdu_buffer, 0, APDU_CODE_COMMAND_NOT_ALLOWED);
+    io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
 }
 
 __Z_INLINE void app_reply_key() {
-  set_code(G_io_apdu_buffer, key_state.len, APDU_CODE_OK);
-  io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, key_state.len + 2);
+    set_code(G_io_apdu_buffer, key_state.len, APDU_CODE_OK);
+    io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, key_state.len + 2);
 }
 
 __Z_INLINE void app_reply_address() {
-  set_code(G_io_apdu_buffer, action_addrResponse.len, APDU_CODE_OK);
-  io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, action_addrResponse.len + 2);
+    set_code(G_io_apdu_buffer, action_addrResponse.len, APDU_CODE_OK);
+    io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, action_addrResponse.len + 2);
 }
 
 __Z_INLINE void app_reply_error() {
-  MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
-  set_code(G_io_apdu_buffer, 0, APDU_CODE_DATA_INVALID);
-  io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
+    MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
+    set_code(G_io_apdu_buffer, 0, APDU_CODE_DATA_INVALID);
+    io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
 }
 
 __Z_INLINE void app_reply_hash() {
-  view_tx_state();
-  set_code(G_io_apdu_buffer, 32, APDU_CODE_OK);
-  io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 32 + 2);
+    view_tx_state();
+    set_code(G_io_apdu_buffer, 32, APDU_CODE_OK);
+    io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 32 + 2);
 }
--- app/src/common/main.c
+++ app/src/common/main.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *   (c) 2016 Ledger
- *   (c) 2018 -2022 Zondax AG
+ *   (c) 2018 - 2024 Zondax AG
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,25 +14,27 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  ********************************************************************************/
+#include <os_io_seproxyhal.h>
+
 #include "app_main.h"
 #include "view.h"
 
-#include <os_io_seproxyhal.h>
-
 __attribute__((section(".boot"))) int main(void) {
-  // exit critical section
-  __asm volatile("cpsie i");
+    // exit critical section
+    __asm volatile("cpsie i");
 
-  view_init();
-  os_boot();
+    view_init();
+    os_boot();
 
-  BEGIN_TRY {
-    TRY {
-      app_init();
-      app_main();
+    BEGIN_TRY {
+        TRY {
+            app_init();
+            app_main();
+        }
+        CATCH_OTHER(e) {
+        }
+        FINALLY {
+        }
     }
-    CATCH_OTHER(e) {}
-    FINALLY {}
-  }
-  END_TRY;
+    END_TRY;
 }
