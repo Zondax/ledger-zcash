e4131d81e9978102e2129f77cb2a1c7710d0d9e8
emmanuelm41 <emmanuelm41@gmail.com>
2024-07-30 09:33:50 -0300

    feat: add flex support WIP

--- app/src/common/tx.h
+++ app/src/common/tx.h
@@ -19,7 +19,7 @@
 #include "os.h"
 #include "zxerror.h"
 
-#if defined(TARGET_NANOX) || defined(TARGET_NANOS2) || defined(TARGET_STAX)
+#if defined(TARGET_NANOX) || defined(TARGET_NANOS2) || defined(TARGET_STAX) || defined(TARGET_FLEX)
 #define RAM_BUFFER_SIZE   8192
 #define FLASH_BUFFER_SIZE 16384
 #elif defined(TARGET_NANOS)
